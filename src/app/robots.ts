e4e488b0b2fa299da6ccf6a714cbe3eb4315132b
JamesCoder1688 <HNCDEA@outlook.com>
2025-06-29 16:38:10 +0800

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/', '/static/'],
     },
-    sitemap: 'https://www.bac-calculator.online/sitemap.xml',
+    sitemap: 'https://www.baccalcutor.cc/sitemap.xml',
   }
 }
